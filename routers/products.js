751a44ac9ff1f54d648201efa9d61b62711293c2
AngkurMondal <angkur002@gmail.com>
2024-05-24 22:29:53 +0600

    organized the files and added README file

---
+++ routers/products.js
@@ -0,0 +1,34 @@
+// const {Product} = require('../models/product.js');
+const Product = require('../models/product.js');
+const express = require('express');
+const router = express.Router();
+
+router.get(`/`, async (req, res) => {
+    const products = await Product.find();
+
+    if (!products) {
+        res.status(500).json({
+            success: false
+        });
+    }
+    res.send(products);
+});
+
+router.post(`/`, (req, res) => {
+    const product = new Product({
+        name: req.body.name,
+        image: req.body.image,
+        countInStock: req.body.countInStock
+    });
+    product.save()
+        .then((createdProduct) => res.status(201).json({
+            data: createdProduct,
+            success: true
+        }))
+        .catch((err) => res.status(500).json({
+            error: err,
+            success: false
+        }));
+});
+
+module.exports = router;
